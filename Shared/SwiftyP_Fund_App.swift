7ba20be0db7569a41d8676641e436f2924b34a25
lukasbuse <lukasbuse4@gmail.com>
2022-07-27 09:16:58 +0300

    Initial Commit

---
+++ Shared/SwiftyP_Fund_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyP_Fund_App.swift
+//  Shared
+//
+//  Created by Josh Buse on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyP_Fund_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
