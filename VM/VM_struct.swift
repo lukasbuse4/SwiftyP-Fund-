f82f1cd518755923c98e8606467aa5b8671ce95d
lukasbuse <lukasbuse4@gmail.com>
2022-07-27 11:01:11 +0300

    Struct B- created VM & View for Struct P

---
+++ VM/VM_struct.swift
@@ -0,0 +1,12 @@
+//
+//  VM_struct.swift
+//  SwiftyP(Fund) (iOS)
+//
+//  Created by Josh Buse on 7/27/22.
+//
+
+import Foundation
+
+//Create new VM for struct practice
+// do in Model (connect through VM)
+// VM connect View_struct ---> for UI (Frontend)
